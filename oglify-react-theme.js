ff404dce941aea62ed9549c1e6c6172607a0ac31
brendansafr <brendansafr@gmail.com>
2022-01-15 21:15:01 -0500

    Add bulma

--- oglify-react-theme.js
+++ oglify-react-theme.js
@@ -2,4 +2,6 @@
 
 // Variables exported by this module can be imported by other packages and
 // applications. See oglify-react-theme-tests.js for an example of importing.
-export const name = 'oglify-react-theme';
+export const name = "oglify-react-theme";
+
+import "bulma/css/bulma.min.css";
